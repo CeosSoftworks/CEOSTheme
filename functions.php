ea7fa4c4695cd6be86f525a0349553e5150e68db
Jeferson dos Santos Oliveira <jefersong1@yahoo.com.br>
2015-09-02 20:10:06 -0300

    First draft of CEOS Augment Client

--- functions.php
+++ functions.php
@@ -51,6 +51,7 @@ function enqueueScripts(){
 	wp_enqueue_script('main', "{$templateDir}/js/main.js");
 	wp_enqueue_script('ceos-augment', "{$templateDir}/js/ceos-augment.js");
 	wp_enqueue_script('ceos-augment-arrays', "{$templateDir}/js/ceos-augment-arrays.js");
+	wp_enqueue_script('ceos-augment-client', "{$templateDir}/js/ceos-augment-client.js");
 	wp_enqueue_script('ceos-parallax', "{$templateDir}/js/ceos-parallax.js");
 }
 
---
+++ js/ceos-augment-client.js
@@ -0,0 +1,10 @@
+"use strict"
+
+var __ = ceosAugment = ceosAugment || {};
+    __.client = __.client || {};
+    
+__.client.getOS = function () {}
+__.client.getBrowser = function () {}
+__.client.getLanguage = function () {}
+
+
